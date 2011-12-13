09ca44b34fc123e67e0dd4ae03d6d2102805cb85
Christian Krause <kizkizzbangbang@googlemail.com>
2011-12-13 16:09:04 +0100

    simple build definition and better README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Automata Simulator
+Automata Simulator
+==================
+
+This project provides an API and a simulator for automatas like finite-state
+machines which is intended to be a helpful learning tool for computer science
+students.
+
---
+++ project/Build.scala
@@ -0,0 +1,35 @@
+import sbt._
+import Keys._
+
+import Dependencies._
+import BuildSettings._
+
+object BuildSettings {
+  val buildOrganization = "com.github.scalomator"
+  val buildVersion      = "0.1-SNAPSHOT"
+  val buildScalaVersion = "2.9.1"
+
+  val buildSettings = Defaults.defaultSettings ++ Seq (
+    organization := buildOrganization,
+    version      := buildVersion,
+    scalaVersion := buildScalaVersion
+  )
+}
+
+object ScalomatorBuild extends Build {
+
+  // -----------------------------------------------------------------------
+  // project definition
+  // -----------------------------------------------------------------------
+
+  lazy val root = Project ( "scalomator", file ("."),
+    settings = buildSettings ++ Seq (
+      libraryDependencies ++= Seq ( specs2 )
+    )
+  )
+
+}
+
+object Dependencies {
+  val specs2 = "org.specs2" %% "specs2" % "1.6.1" % "test"
+}
