815a406544d2bc687d4b44f772277171b4b3d370
Nils Foken <nf462138@gmail.com>
2012-01-25 20:36:58 +0100

    finished state changes through popup menu, added medthods to extract all state and transition data for scala to import for running and saving, added helpful hints for later JTree addition to the right-hand panel (Peter maybe? :))

--- client-swing/src/main/java/scalax/automata/gui/GUI.java
+++ client-swing/src/main/java/scalax/automata/gui/GUI.java
@@ -1,6 +1,7 @@
 package main.java.scalax.automata.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,9 +9,11 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -33,15 +36,22 @@ import com.mxgraph.view.mxGraph;
 @SuppressWarnings("serial")
 public class GUI extends JFrame {
 	private static final int CELL_RADIUS = 80;
-	private final static int INITIAL_STATE = 0;
-	private final static int NORMAL_STATE = 1;
-	private final static int END_STATE = 2;
+	private final static int INITIAL_STATE = 1;
+	private final static int NORMAL_STATE = 2;
+	private final static int END_STATE = 4;
 
 	public mxGraph graph;
 	public Object root;
+	
+	private HashMap<String, String> initialState = new HashMap<String, String>();
+	private ArrayList<HashMap<String, String>> endStates = new ArrayList<HashMap<String, String>>();
+	private ArrayList<HashMap<String, String>> states = new ArrayList<HashMap<String, String>>();
+	private ArrayList<HashMap<String, String>> transitions = new ArrayList<HashMap<String, String>>();
 
+	private Component top;
 	private int nextInt = 1;
-	private JPopupMenu popup;	
+	private boolean hasInitialState = false;
+	private JPopupMenu popup;
 	private Point popupPosition = new Point();
 	private mxGraphComponent graphComponent = null;
 	private JTextField alphabetField, testField;
@@ -49,6 +59,7 @@ public class GUI extends JFrame {
 	
 	public GUI (String name) {
 		super(name);
+		top = getContentPane();
 	}
 	
 	public void initGUI(JFrame gui) {
@@ -60,7 +71,6 @@ public class GUI extends JFrame {
 
 	    //Create the pop-up menu.
 	    popup = new JPopupMenu();
-		// TODO: create image for icon
 	    menuItem = new JMenuItem("Add state");
 	    menuItem.addActionListener(new ActionListener() {
 			@Override
@@ -72,7 +82,6 @@ public class GUI extends JFrame {
 	    });
 	    popup.add(menuItem);
 	    
-		// TODO: create image for icon
 	    menuItem = new JMenuItem("Remove");
 	    menuItem.addActionListener(new ActionListener() {
 			@Override
@@ -82,18 +91,55 @@ public class GUI extends JFrame {
 			}
 	    });
 	    popup.add(menuItem);
+	    popup.addSeparator();
 	    
-		// TODO: create image for icon
-	    menuItem = new JMenuItem("Change state");
-	    // TODO: implement change state
+	    menuItem = new JMenuItem("Change to Initial state");
+	    menuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if (hasInitialState) {
+					JOptionPane.showMessageDialog(top, "There can only be one initial state!\n" +
+							"Remove or change the existing initial state first.", "Warning", JOptionPane.WARNING_MESSAGE);
+				}
+				else {
+					hasInitialState = true;
+					Object cell = graphComponent.getCellAt(popupPosition.x, popupPosition.y);
+					graph.getModel().setStyle(cell, "shape=initialShape;perimeter=ellipsePerimeter");
+					graphComponent.refresh();
+				}
+			}
+	    });
+	    popup.add(menuItem);
+	    
+	    menuItem = new JMenuItem("Change to Normal state");
+	    menuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				hasInitialState = false;
+				Object cell = graphComponent.getCellAt(popupPosition.x, popupPosition.y);
+				graph.getModel().setStyle(cell, "shape=ellipse;perimeter=ellipsePerimeter");
+				graphComponent.refresh();
+			}
+	    });
+	    popup.add(menuItem);
+	    
+	    menuItem = new JMenuItem("Change to End state");
+	    menuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Object cell = graphComponent.getCellAt(popupPosition.x, popupPosition.y);
+				graph.getModel().setStyle(cell, "shape=doubleEllipse;perimeter=ellipsePerimeter");
+				graphComponent.refresh();
+			}
+	    });
 	    popup.add(menuItem);
 		
 		// menus
 		menu = new JMenu("File");
 		menu.setMnemonic(KeyEvent.VK_F);
 		menuBar.add(menu);
-		// TODO: create image for icon
-		menuItem = new JMenuItem("New", new ImageIcon());
+
+		menuItem = new JMenuItem("New");
 		menuItem.setMnemonic(KeyEvent.VK_N);
 	    menuItem.addActionListener(new ActionListener() {
 			@Override
@@ -103,19 +149,18 @@ public class GUI extends JFrame {
 	    });
 		menu.add(menuItem);
 		
-		// TODO: create image for icon
-		menuItem = new JMenuItem("Open", new ImageIcon());
+		menuItem = new JMenuItem("Open");
 		menuItem.setMnemonic(KeyEvent.VK_O);
 	    menuItem.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
+				removeAllCells();
 				loadAutomata();
 			}
 	    });
 		menu.add(menuItem);
 		
-		// TODO: create image for icon
-		menuItem = new JMenuItem("Save", new ImageIcon());
+		menuItem = new JMenuItem("Save");
 		menuItem.setMnemonic(KeyEvent.VK_S);
 	    menuItem.addActionListener(new ActionListener() {
 			@Override
@@ -126,8 +171,7 @@ public class GUI extends JFrame {
 		menu.add(menuItem);
 		menu.addSeparator();
 		
-		// TODO: create image for icon
-		menuItem = new JMenuItem("Quit", new ImageIcon());
+		menuItem = new JMenuItem("Quit");
 		menuItem.setMnemonic(KeyEvent.VK_Q);
 	    menuItem.addActionListener(new ActionListener() {
 			@Override
@@ -141,8 +185,7 @@ public class GUI extends JFrame {
 		menu.setMnemonic(KeyEvent.VK_S);
 		menuBar.add(menu);
 		
-		// TODO: create image for icon
-		menuItem = new JMenuItem("Run", new ImageIcon());
+		menuItem = new JMenuItem("Run");
 		menuItem.setMnemonic(KeyEvent.VK_R);
 	    menuItem.addActionListener(new ActionListener() {
 			@Override
@@ -156,8 +199,7 @@ public class GUI extends JFrame {
 		menu.setMnemonic(KeyEvent.VK_I);
 		menuBar.add(menu);
 		
-		// TODO: create image for icon
-		menuItem = new JMenuItem("About", new ImageIcon());
+		menuItem = new JMenuItem("About");
 		menuItem.setMnemonic(KeyEvent.VK_A);
 	    menuItem.addActionListener(new ActionListener() {
 			@Override
@@ -212,11 +254,13 @@ public class GUI extends JFrame {
 		
 		sidePanel.add(subPanel, BorderLayout.NORTH);
 		sidePanel.add(buttonPanel, BorderLayout.SOUTH);
+		// TODO: add a JTree (for example) for the states and their data (label, x, y)
 		subPanel.add(statesPanel);
+		// TODO: add a JTree (for example) for the edges and their data (input, source, target)
 		subPanel.add(transitionsPanel);
 		subPanel.add(infoPanel);
 		
-		status = new JLabel();
+		status = new JLabel("");
 		statusBar.add(status);	
 		
 		gui.setJMenuBar(menuBar);
@@ -246,10 +290,9 @@ public class GUI extends JFrame {
 	        @Override
 	        public void invoke(Object sender, mxEventObject evt) {
 	            if (sender instanceof mxGraph) {
-	            	System.out.print("Movement happened for: ");
 	                for (Object cell : ((mxGraph)sender).getSelectionCells()) {
-	                	// TODO: update cell geometry attributes in appropriate lists 
-	                    System.out.println("cell=" + graph.getLabel(cell));
+	                	// TODO: update cell geometry attributes in appropriate list
+	                	// (if at all displayed in the list, but should if it is a JTree as proposed)
 	                }
 	            }
 	        }
@@ -276,20 +319,27 @@ public class GUI extends JFrame {
 									graph.getModel().setValue(cell, new StringBuilder("S_").append(nextInt++));
 								}
 							}
-
+							
 							// TODO: add cells to appropriate lists
 							Object shape = graph.getCellStyle(cell).get("shape");
 							if (shape.toString().equals("initialShape")) {
-								System.out.println("init added");
+								if (hasInitialState) {
+									// change potential initial state to a normal state
+									graph.getModel().setStyle(cell, "shape=ellipse;perimeter=ellipsePerimeter");
+									// add a normal state to list
+								}
+								else {
+									// add an initial state to list
+								}
 							}
 							else if (shape.toString().equals("ellipse")) {
-								System.out.println("normal added");
+								// add a normal state to list
 							}
 							else if (shape.toString().equals("doubleEllipse")) {
-								System.out.println("end added");
+								// add an end state to list
 							}
 							else if (shape.toString().equals("connector")) {
-								System.out.println("connector added");
+								// add an edge to list
 							}
 						}
 	                }
@@ -308,16 +358,17 @@ public class GUI extends JFrame {
 							// TODO: remove cells from appropriate lists
 							Object shape = graph.getCellStyle(cell).get("shape");
 							if (shape.toString().equals("initialShape")) {
-								System.out.println("init removed");
+								hasInitialState = false;
+								// remove initial state from list
 							}
 							else if (shape.toString().equals("ellipse")) {
-								System.out.println("normal removed");
+								// remove normal state from list
 							}
 							else if (shape.toString().equals("doubleEllipse")) {
-								System.out.println("end removed");
+								// remove end state from list
 							}
 							else if (shape.toString().equals("connector")) {
-								System.out.println("connector removed");
+								// remove edge from list
 							}
 						}
 	                }
@@ -339,17 +390,35 @@ public class GUI extends JFrame {
 	    					popup.getComponent(0).setEnabled(false);
 	    					popup.getComponent(1).setEnabled(true);
 	    					popup.getComponent(2).setEnabled(false);
+	    					popup.getComponent(3).setEnabled(false);
+	    					popup.getComponent(4).setEnabled(false);
+	    					popup.getComponent(5).setEnabled(false);
 	    				}
 	    				else {
 	    					popup.getComponent(0).setEnabled(false);
 	    					popup.getComponent(1).setEnabled(true);
 	    					popup.getComponent(2).setEnabled(true);
+	    					if (shape.toString().equals("initialShape"))
+	    						popup.getComponent(3).setEnabled(false);
+	    					else
+	    						popup.getComponent(3).setEnabled(true);
+	    					if (shape.toString().equals("ellipse"))
+	    						popup.getComponent(4).setEnabled(false);
+	    					else
+	    						popup.getComponent(4).setEnabled(true);
+	    					if (shape.toString().equals("doubleEllipse"))
+	    						popup.getComponent(5).setEnabled(false);
+	    					else
+	    						popup.getComponent(5).setEnabled(true);
 	    				}
 	    			}
 	    			else {
 	    				popup.getComponent(0).setEnabled(true);
 	    				popup.getComponent(1).setEnabled(false);
 	    				popup.getComponent(2).setEnabled(false);
+	    				popup.getComponent(3).setEnabled(false);
+	    				popup.getComponent(4).setEnabled(false);
+	    				popup.getComponent(5).setEnabled(false);
 	    			}
 	    			popupPosition = e.getPoint();
     				popup.show(e.getComponent(), e.getX(), e.getY());
@@ -372,6 +441,7 @@ public class GUI extends JFrame {
 			case INITIAL_STATE:
 				state = graph.insertVertex(root, null, name, x, y, radius,
 						radius, "shape=initialShape;perimeter=ellipsePerimeter");
+				hasInitialState = true;
 				break;
 			case NORMAL_STATE:
 				state = graph.insertVertex(root, null, name, x, y, radius,
@@ -382,7 +452,6 @@ public class GUI extends JFrame {
 						radius, "shape=doubleEllipse;perimeter=ellipsePerimeter");
 				break;
 			}
-			System.out.println(nextInt);
 			nextInt++;
 		}
 		finally
@@ -414,7 +483,7 @@ public class GUI extends JFrame {
 	public void loadAutomata() {
 		// TODO: loading from file or scala here
 		System.out.println("some loading happens here");
-		// FIXME: remove test automata
+		// FIXME: remove test automata with loaded stuff
 		Object v1 = addState("first", 50, 50, CELL_RADIUS, INITIAL_STATE);
 		Object v2 = addState("second", 350, 50, CELL_RADIUS, NORMAL_STATE);
 		Object v3 = addState("third", 200, 200, CELL_RADIUS, END_STATE);
@@ -424,17 +493,85 @@ public class GUI extends JFrame {
 	}
 	
 	public void saveAutomata() {
-		// TODO: grab states and transitions from list or graph
-		System.out.println("some saving happens here");
+		if (!hasInitialState) {
+			JOptionPane.showMessageDialog(top, "You need an initial state to save the automata!", "Warning", JOptionPane.WARNING_MESSAGE);
+		}
+		else {
+			extractData();
+			// TODO: grab states and transitions through public methods
+			System.out.println("some saving happens here");
+		}
 	}
 	
 	public void runSimulation() {
-		// TODO: run the simulation
-		System.out.println("some simulation running happens here");
+		if (!hasInitialState) {
+			JOptionPane.showMessageDialog(top, "You need an initial state to run the simulation!", "Warning", JOptionPane.WARNING_MESSAGE);
+		}
+		else {
+			// TODO: run the simulation
+			System.out.println("some simulation running happens here");
+		}
+	}
+	
+	void extractData() {
+		Object[] vertices = graph.getChildCells(root, true, false);
+		Object[] edges = graph.getChildCells(root, false, true);
+		
+		initialState = new HashMap<String, String>();
+		endStates = new ArrayList<HashMap<String, String>>();
+		states = new ArrayList<HashMap<String, String>>();
+		transitions = new ArrayList<HashMap<String, String>>();
+		
+		for (Object vertex : vertices) {
+			Object cell = graph.getCellStyle(vertex).get("shape");
+			if (cell.toString().equals("initialShape")) {
+				initialState.put("x", String.valueOf(graph.getCellGeometry(vertex).getX()));
+				initialState.put("y", String.valueOf(graph.getCellGeometry(vertex).getY()));
+				initialState.put("name", String.valueOf(graph.getLabel(vertex)));
+			}
+			else if (cell.toString().equals("doubleEllipse")) {
+				HashMap<String, String> endState = new HashMap<String, String>();
+				endState.put("x", String.valueOf(graph.getCellGeometry(vertex).getX()));
+				endState.put("y", String.valueOf(graph.getCellGeometry(vertex).getY()));
+				endState.put("name", String.valueOf(graph.getLabel(vertex)));
+				endStates.add(endState);
+			}
+			else if (cell.toString().equals("ellipse")) {
+				HashMap<String, String> state = new HashMap<String, String>();
+				state.put("x", String.valueOf(graph.getCellGeometry(vertex).getX()));
+				state.put("y", String.valueOf(graph.getCellGeometry(vertex).getY()));
+				state.put("name", String.valueOf(graph.getLabel(vertex)));
+				states.add(state);
+			}
+		}
+		
+		for (Object edge : edges) {
+			HashMap<String, String> transition = new HashMap<String, String>();
+			transition.put("source", String.valueOf(graph.getLabel(((mxCell)edge).getSource())));
+			transition.put("target", String.valueOf(graph.getLabel(((mxCell)edge).getTarget())));
+			transition.put("input", String.valueOf(graph.getLabel(edge)));
+			transitions.add(transition);
+		}
+	}
+	
+	HashMap<String, String> getInitialState() {
+		return initialState;
+	}
+	
+	ArrayList<HashMap<String, String>> getEndStates() {
+		return endStates;
+	}
+	
+	ArrayList<HashMap<String, String>> getStates() {
+		return states;
+	}
+	
+	ArrayList<HashMap<String, String>> getTransitions() {
+		return transitions;
 	}
 	
 	public void displayAbout() {
-		JOptionPane.showMessageDialog(this, "Some About message", "About", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(top, "Some About message", "About", JOptionPane.INFORMATION_MESSAGE);
 	}
 	
 	public void quit() {
@@ -460,15 +597,14 @@ public class GUI extends JFrame {
 	public static void main(String[] args) {
 		GUI gui = new GUI("Scalomator - Simulate finite-state machines");
 		gui.initGUI(gui);
-		// FIXME: remove test load
+		
+		// FIXME: remove test load and bogus status message
 		gui.loadAutomata();
+		gui.setStatusMessage("This is some status message.");
+
 		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gui.setSize(640, 480);
 		gui.setVisible(true);
-		
-		// some test for vertex and edge grabbing
-//		System.out.println(Arrays.toString(gui.graph.getChildCells(gui.root, true, false)));
-//		System.out.println(Arrays.toString(gui.graph.getChildCells(gui.root, false, true)));
 	}
 
 }
