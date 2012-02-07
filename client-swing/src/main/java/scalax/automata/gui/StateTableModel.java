93d98582522e97fe30426edd9932a26ee7af6a0b
Nils Foken <nf462138@gmail.com>
2012-02-07 19:05:07 +0100

    added license for JGRAPH and copied the header to all java files

--- client-swing/src/main/java/scalax/automata/gui/StateTableModel.java
+++ client-swing/src/main/java/scalax/automata/gui/StateTableModel.java
@@ -1,3 +1,30 @@
+/* **************************************************************************
+ *                                                                          *
+ *  Copyright (C)  2011-2012  Peter Kossek, Nils Foken, Christian Krause    *
+ *                                                                          *
+ *  Peter Kossek     <peter.kossek@it2009.ba-leipzig.de>                    *
+ *  Nils Foken       <nils.foken@it2009.ba-leipzig.de>                      *
+ *  Christian Krause <christian.krause@it2009.ba-leipzig.de>                *
+ *                                                                          *
+ ****************************************************************************
+ *                                                                          *
+ *  This file is part of 'scalomator'.                                      *
+ *                                                                          *
+ *  This project is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  any later version.                                                      *
+ *                                                                          *
+ *  This project is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this project. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
 package main.java.scalax.automata.gui;
 
 import java.util.ArrayList;
